6e472c816538e5cd00c2c1ec1033d3f17f6a4d7d
sanjupriya-manikyala <86363848+sanjupriya-manikyala@users.noreply.github.com>
2021-08-11 22:22:25 +0530

    Add files via upload

---
+++ ConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using CoreServices.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreServices.Tests
+{
+    class ConnectionFactory
+    {
+        public ProductDBContext CreateContextForInMemory()
+        {
+            var option = new DbContextOptionsBuilder<ProductDBContext>().UseInMemoryDatabase(databaseName: "ProductDB").Options;
+
+            var context = new ProductDBContext(option);
+            if (context != null)
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+
+            return context;
+        }
+    }
+}
