84bad77650ed73e05bde53c8eb4532091909d5ea
sanjupriya-manikyala <86363848+sanjupriya-manikyala@users.noreply.github.com>
2021-08-13 16:57:41 +0530

    Service and Repository tests

---
+++ ProductRepositoryTests.cs
@@ -0,0 +1,37 @@
+﻿using CoreServices.Models;
+using CoreServices.Repository;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace CoreServices.Tests
+{
+    public class ProductRepositoryTests
+    {
+        private readonly Mock<ProductDBContext> _dbContext;
+        private readonly ProductRepository _productRepository;
+        private readonly Mock<DbSet<Product>> _mockProducts;
+
+        public ProductRepositoryTests()
+        {
+            _dbContext = new Mock<ProductDBContext>();
+            _productRepository = new ProductRepository(_dbContext.Object);
+            _mockProducts = new Mock<DbSet<Product>>();
+
+        }
+
+        [Fact]
+        public async void AddAsync_PassedValidData_Return_OkResult()
+        {
+            //Arrange
+            var product = new Product { Id = 6, Name = "Test Name6", Price = 670 };
+            _dbContext.Setup(p => p.Products.Add(product));
+
+            //Act
+            var data = await _productRepository.AddAsync(product);
+
+            //Assert
+            Assert.Equal(product, data);
+        }
+    }
+}
---
+++ ProductServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using CoreServices.DTO;
+using CoreServices.Models;
+using CoreServices.Repository;
+using CoreServices.Services;
+using Moq;
+using Xunit;
+
+namespace CoreServices.Tests
+{
+    public class ProductServiceTests
+    {
+        private readonly Mock<IRepository> _repository;
+        private readonly ProductService _productService;
+
+        public ProductServiceTests()
+        {
+            _repository = new Mock<IRepository>();
+            _productService = new ProductService(_repository.Object);
+        }
+
+        [Fact]
+        public async void AddAsync_PassedValidData_Return_OkResult()
+        {
+            //Arrange
+            var dto = new ProductDTO() { Id = 6, Name = "Test Name6", Price = 670 };
+            var product = new Product { Id = dto.Id, Name = dto.Name, Price = dto.Price };
+            _repository.Setup(p => p.AddAsync(product)).ReturnsAsync(product);
+
+            //Act
+            var data = await _productService.AddAsync(dto);
+
+            //Assert
+            Assert.Equal(dto, data);
+        }
+    }
+}
