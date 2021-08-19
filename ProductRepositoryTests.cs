3fc6ec61f93fac3e386fa7de58c3ed4650b875c5
sanjupriya-manikyala <86363848+sanjupriya-manikyala@users.noreply.github.com>
2021-08-19 15:33:20 +0530

    in-memory db

--- ProductRepositoryTests.cs
+++ ProductRepositoryTests.cs
@@ -1,79 +1,29 @@
 ﻿using AutoFixture;
 using CoreServices.Models;
-using CoreServices.Repository;
-using Moq;
 using Xunit;
-using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
-using AutoFixture.AutoMoq;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace CoreServices.Tests
 {
     public class ProductRepositoryTests
     {
-        private readonly Mock<ProductDBContext> _dbContext;
-        private readonly ProductRepository _productRepository;
-        private readonly Mock<DbSet<Product>> _mockproducts;
-
-        public ProductRepositoryTests()
-        {
-            _mockproducts = new Mock<DbSet<Product>>(); 
-            _dbContext = new Mock<ProductDBContext>();
-            _productRepository = new ProductRepository(_dbContext.Object);
-        }
 
         [Fact]
-        public async void AddAsync_PassedValidData_Return_OkResult()
+        public void Add_ValidDataPassed_Return_MatchResult()
         {
-            //Arrange
+            //Arrange  
+            var factory = new ConnectionFactory();
+            var context = factory.CreateContextForInMemory();
             var fixture = new Fixture();
             var product = fixture.Create<Product>();
-            _dbContext.Setup(p => p.AddAsync(product, default)).ReturnsAsync(product);
 
-            //Act
-            var data = await _productRepository.AddAsync(product);
-
-            //Assert
-            Assert.NotNull(data);
-            Assert.IsType<Product>(data);
-        }
-
-        [Fact]
-        public async void AddAsync_PassedValidData_Return_MatchResult()
-        {
-            //Arrange
-            var fixture = new Fixture().Customize(new AutoMoqCustomization());
-            var product = fixture.Create<Product>();
-            _dbContext.Setup(p => p.AddAsync(product,default)).ReturnsAsync(product);
-
-
-            //Act
-            var data = await _productRepository.AddAsync(product);
+            //Act  
+            var data = context.Products.Add(product);
+            context.SaveChanges();
 
             //Assert
             Assert.NotNull(data);
-            Assert.IsType<Product>(data);
-            Assert.Equal(product, data);
-        }
-
-        [Fact]
-        public async void AddAsync_PassedInValidData_Return_BadRequest()
-        {
-            //Arrange
-            var fixture = new Fixture();
-            var product = fixture.Create<Product>();
-            var exception = fixture.Create<Exception>();
-            _dbContext.Setup(p => p.AddAsync(product, default)).ThrowsAsync(exception);
-
-            //Act
-            var data = await Assert.ThrowsAsync<Exception>(() => _productRepository.AddAsync(product));
-
-            //Assert
-            Assert.NotNull(data);
-            Assert.IsType<Exception>(data);
-            Assert.Equal(exception.Message, data.Message);
+            Assert.Equal(product, data.Entity);
         }
     }
 }
+            
